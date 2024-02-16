574a29776d8017f38590b20cdce556d8b84373e5
Sreenaina Koujala <nkoujala@gmail.com>
2024-02-16 16:22:46 -0500

    updates

--- src/services/coursesService.js
+++
@@ -1,9 +0,0 @@
-const courses = {
-    async getCourses() {
-        let response = await fetch('/api/courses');
-        let data = await response.json();
-        return data;
-    },
-};
-
-export default courses;
