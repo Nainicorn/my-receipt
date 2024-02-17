680060dee3c2cfd5704af5172ad26a29c5a48719
Sreenaina Koujala <nkoujala@gmail.com>
2024-02-17 15:46:59 -0500

    updates...

--- my-receipt/app/login/page.js
+++ my-receipt/app/login/page.js
@@ -1,8 +1,16 @@
 export default function Login() {
-    return (
-      <main>
-        Login - Naina and Thierry
-      </main>
-    );
-  }
-  
+  return (
+    <main>
+      <div className="login-box">
+        <div className="login-title">Login</div>
+        <div className="login-username">Username</div>
+        <div className="login-password">Password</div>
+        <div>
+          <div className="login-remember">Remember Me</div>
+          <div className="login-forgot">Forgot Password?</div>
+        </div>
+        <div className="login-btn">Login!</div>
+      </div>
+    </main>
+  );
+}
--- my-receipt/app/page.js
+++ my-receipt/app/page.js
@@ -1,14 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import graphic from './graphic2.jpg'
+
 export default function Home() {
   return (
     <main>
-      <div className="section-1">
-        <div>Welcome to My Receipt App</div>
-      </div>
-      <div className="section-2">
-        <div className="section-div">Get Started!</div>
-        <button className="sign-up-btn">Sign Up</button>
-        <button className="login-btn">Login</button>
+      <nav>
+        <img src="favicon.ico" />
+        <h3 className="title">My Receipt</h3>
+        <div className="btn">Sign Up</div>
+        <div className="btn"><Link href="/login" style={{ textDecoration: 'none', color: 'white' }}>Login</Link></div>
+      </nav>
+      <div className="section">
+        <div className="section-1">
+          <h1>Go Paperless.</h1>
+          <div className="description">Digitize and simplify your expense tracking with this personalized receipt manager. Track, categorize, and ensure you never lose a receipt again. View detailed insights into your financial habits across various categories, universally compatible with any company or chain!</div>
+        </div>
+        <div className="section-2">
+          <Image className="graphic" src={graphic} />
+        </div>
       </div>
     </main>
   );
 }
+
