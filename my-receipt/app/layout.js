9e0aad759d746878af67e1faac7903d4d565ff44
Sreenaina Koujala <nkoujala@gmail.com>
2024-02-17 12:57:59 -0500

    updates

--- my-receipt/app/layout.js
+++ my-receipt/app/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "./root.scss";
 
 export default function RootLayout({ children }) {
   return (
--- my-receipt/app/page.js
+++ my-receipt/app/page.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <main>
-      Home - Naina and Thierry
+      <div className="section-1">
+        <div>Welcome to My Receipt App</div>
+      </div>
+      <div className="section-2">
+        <div className="section-div">Get Started!</div>
+        <button className="sign-up-btn">Sign Up</button>
+        <button className="login-btn">Login</button>
+      </div>
     </main>
   );
 }
