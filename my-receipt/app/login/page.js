f1336dbee11e579783aab089012671abc93241ca
Sreenaina Koujala <nkoujala@gmail.com>
2024-02-17 17:24:43 -0500

    updates...

--- my-receipt/app/login/page.js
+++ my-receipt/app/login/page.js
@@ -2,15 +2,17 @@ export default function Login() {
   return (
     <main>
       <div className="login-box">
-        <div className="login-title">Login</div>
-        <div className="login-username">Username</div>
-        <div className="login-password">Password</div>
-        <div>
+        <h3 className="login-title">Login</h3>
+        <input className="login-username" type="text" placeholder="Username" class="email" />
+        <input className="login-password" type="password" placeholder="Password" class="pass" />
+        <div className="login-extras">
+          <input type="checkbox"/>
           <div className="login-remember">Remember Me</div>
-          <div className="login-forgot">Forgot Password?</div>
         </div>
         <div className="login-btn">Login!</div>
       </div>
     </main>
   );
 }
+
+
