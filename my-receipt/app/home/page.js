5405c056ddef0dee9f6edab042cd2f6b365809a4
Sreenaina Koujala <nkoujala@gmail.com>
2024-02-18 12:02:58 -0500

    UPDATES

--- my-receipt/app/home/page.js
+++ my-receipt/app/home/page.js
@@ -10,37 +10,32 @@ export default function Home() {
       <nav className="home-nav">
         <div className="home-display-1">
           <Image className="home-img" src={icon} />
-          <h3 className="home-title">Welcome User123456</h3>
-        </div>
-        <div class="dropdown">
-          <button class="dropbtn">Select Year</button>
-          <div class="dropdown-content">
-            <a href="#">2023</a>
-            <a href="#">2022</a>
-            <a href="#">2021</a>
-          </div>
+          <div className="home-title">Welcome, User123456</div>
         </div>
         <div className="logout-btn">Log Out</div>
       </nav>
       <div className="home-section">
         <div className="home-section-1">
           <div className="home-section-title">
-            <h1 className="home-receipt-title">Your Receipts</h1>
+            <div className="home-receipt-title">Your Receipts</div>
             <div class="dropdown">
               <button class="dropbtn">Filter</button>
               <div class="dropdown-content">
-                <a href="#">Month - Nov</a>
+                <a href="#">Year - 2024</a>
+                <a href="#">Month - January</a>
                 <a href="#">Type - Shopping</a>
               </div>
             </div>
           </div>
           <div className="home-section-content">
             <div className="content-shop">
+              <div className="shop-title">Shopping</div>
               <div className="home-item">Walmart</div>
               <div className="home-item">TJMaxx</div>
               <div className="home-item">Five Below</div>
             </div>
             <div className="content-food">
+            <div className="food-title">Food</div>
               <div className="home-item">Chipotle</div>
               <div className="home-item">Starbucks</div>
               <div className="home-item">Panera Bread</div>
@@ -49,7 +44,7 @@ export default function Home() {
           <div className="home-section-total">Total - $70.00</div>
         </div>
         <div className="home-section-2">
-          <h1 className="home-receipt-title-2">View Receipts</h1>
+          <div className="home-receipt-title-2">View Receipts</div>
           {/* most recent img of receipt digitized */}
         </div>
       </div>
